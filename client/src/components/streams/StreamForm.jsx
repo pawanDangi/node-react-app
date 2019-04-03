a49f448a94805af1db9d479e7796d1d0cd3e6321
Pawan Dangi <pdangi@intertrust.com>
2019-04-03 11:19:42 +0530

    create stream page design

--- client/src/components/streams/StreamForm.jsx
+++ client/src/components/streams/StreamForm.jsx
@@ -1,60 +1,323 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import {
   Paper,
+  Typography,
   Grid,
-  FormControl,
-  InputLabel,
-  Input
+  TextField,
+  Button,
+  MenuItem,
+  Fab
 } from '@material-ui/core/';
+import { Add as AddIcon } from '@material-ui/icons/';
+import className from 'class-names';
 
 const styles = theme => ({
   root: {
+    marginTop: '30px',
+    '& div': {
+      background: '#fff'
+    }
+  },
+  page: {
     ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
-    background: '#f4f4f4',
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+      margin: '0 5%'
+    },
+    [theme.breakpoints.up('sm')]: {
+      width: '90%',
+      margin: '0 5%'
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '80%',
+      margin: '0 10%'
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '70%',
+      margin: '0 15%'
+    },
+    padding: '15px !important'
+  },
+  heading: {
+    borderBottom: '2px solid rgba(0, 0, 0, 0.2)',
+    paddingBottom: 20,
     fontSize: '18px'
   },
+  title: {
+    padding: '10px 0px !important',
+    color: '#1565C0',
+    fontWeight: 600,
+    fontSize: '18px'
+  },
+  form: {
+    padding: '20px 0 0'
+  },
+  grid: {
+    paddingTop: '5px !important',
+    paddingBottom: '5px !important',
+    display: 'flex',
+    alignItems: 'center'
+  },
   textField: {
-    background: '#fff'
+    margin: 0
   },
-  buttons: {
-    textAlign: 'right'
+  validate: {
+    alignItems: 'center',
+    display: 'flex'
   },
-  button: {
-    margin: theme.spacing.unit,
-    '&:focus': {
-      textDecoration: 'none',
-      outline: 'none'
+  validateBtn: {
+    background: '#0D47A1',
+    color: '#fff'
+  },
+  addMarker: {
+    background: '#0D47A1',
+    color: '#fff'
+  },
+  timeBtn: {
+    width: '35%'
+  },
+  or: {
+    width: '100%',
+    textAlign: 'center',
+    borderBottom: '1px solid rgba(0, 0, 0, 0.54)',
+    lineHeight: '0.1em',
+    margin: '10px 0 20px',
+    paddingTop: '15px',
+    fontSize: '15px',
+    '& span': {
+      background: '#fff',
+      color: 'rgba(0, 0, 0, 0.54)',
+      padding: '0 10px'
     }
   }
 });
 
-function StreamForm(props) {
-  const { classes } = props;
+class StreamForm extends Component {
+  state = {};
 
-  return (
-    <Grid container spacing={0}>
-      <Grid item xs={12}>
-        <Paper className={classes.root} elevation={1}>
-          <Grid container spacing={0}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor="component-simple">Url</InputLabel>
-              <Input id="component-simple" />
-            </FormControl>
-          </Grid>
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Paper className={classes.page} elevation={1}>
+          <Typography variant="h5" component="h3" className={classes.heading}>
+            Register Stream
+          </Typography>
+          <form className={classes.form} autoComplete="off">
+            <Grid container spacing={8}>
+              <Grid item xs={12} className={classes.grid}>
+                <Typography
+                  variant="h5"
+                  component="h3"
+                  className={classes.title}
+                >
+                  STREAM DETAILS
+                </Typography>
+              </Grid>
+              <Grid item sm={10} xs={12} className={classes.grid}>
+                <TextField
+                  className={classes.textField}
+                  id="name-input"
+                  label="Name (Optional)"
+                  fullWidth
+                  margin="dense"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item sm={10} xs={12} className={classes.grid}>
+                <TextField
+                  className={classes.textField}
+                  id="url-input"
+                  label="Url"
+                  fullWidth
+                  margin="dense"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid
+                item
+                sm={2}
+                xs={12}
+                className={className(classes.validate, classes.grid)}
+              >
+                <Button variant="contained" className={classes.validateBtn}>
+                  Validate
+                </Button>
+              </Grid>
+              <Grid item sm={10} xs={12} className={classes.grid}>
+                <TextField
+                  className={classes.textField}
+                  id="domain-input"
+                  label="Domain or Bundle (Optional)"
+                  fullWidth
+                  margin="dense"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item sm={5} xs={12} className={classes.grid}>
+                <TextField
+                  className={classes.textField}
+                  id="format-input"
+                  label="Format"
+                  fullWidth
+                  margin="dense"
+                  variant="outlined"
+                  select
+                  value={''}
+                >
+                  <MenuItem key={'hls'} value={'HLS'}>
+                    HLS
+                  </MenuItem>
+                  <MenuItem key={'dash'} value={'DASH'}>
+                    DASH
+                  </MenuItem>
+                </TextField>
+              </Grid>
+              <Grid item sm={5} xs={12} className={classes.grid}>
+                <TextField
+                  className={classes.textField}
+                  id="type-input"
+                  label="Type"
+                  fullWidth
+                  margin="dense"
+                  variant="outlined"
+                  select
+                  value={''}
+                >
+                  <MenuItem key={'vod'} value={'vod'}>
+                    VOD
+                  </MenuItem>
+                  <MenuItem key={'live'} value={'live'}>
+                    LIVE
+                  </MenuItem>
+                  <MenuItem key={'event'} value={'event'}>
+                    EVENT
+                  </MenuItem>
+                </TextField>
+              </Grid>
+              <Grid item sm={10} xs={12} className={classes.grid}>
+                <TextField
+                  className={classes.textField}
+                  id="tags-input"
+                  label="Tags (Optional)"
+                  fullWidth
+                  margin="dense"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12} className={classes.grid}>
+                <Typography
+                  variant="h5"
+                  component="h3"
+                  className={classes.title}
+                >
+                  AD MARKUPS
+                </Typography>
+              </Grid>
+              <Grid item md={2} xs={12} className={classes.grid}>
+                <Typography variant="subheading" component="p">
+                  EP MARKERS :
+                </Typography>
+              </Grid>
+              <Grid item md={8} xs={10} className={classes.grid}>
+                <Grid container spacing={8}>
+                  <Grid item xs={6} className={classes.grid}>
+                    <Typography variant="subheading" component="p">
+                      START
+                    </Typography>
+                    <TextField
+                      id="ep-markers-start-min-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeBtn}
+                      label="min"
+                      type="number"
+                      margin="dense"
+                    />
+                    <TextField
+                      id="ep-markers-start-sec-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeBtn}
+                      label="sec"
+                      type="number"
+                      margin="dense"
+                    />
+                  </Grid>
+                  <Grid item xs={6} className={classes.grid}>
+                    <Typography variant="subheading" component="p">
+                      DURATION
+                    </Typography>
+                    <TextField
+                      id="ep-markers-duration-sec-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeBtn}
+                      type="number"
+                      label="sec"
+                      margin="dense"
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={2} className={classes.grid}>
+                <Fab
+                  size="small"
+                  color="primary"
+                  aria-label="Add"
+                  className={classes.addMarker}
+                >
+                  <AddIcon />
+                </Fab>
+              </Grid>
+              <Grid item md={2} xs={12} className={classes.grid} />
+              <Grid item md={8} xs={10} className={classes.grid}>
+                <h2 className={classes.or}>
+                  <span>OR</span>
+                </h2>
+              </Grid>
+              <Grid item xs={2} className={classes.grid} />
+              <Grid item md={2} xs={12} className={classes.grid} />
+              <Grid item md={8} xs={10} className={classes.grid}>
+                <Grid container spacing={8}>
+                  <Grid item xs={6} className={classes.grid}>
+                    <Typography variant="subheading" component="p">
+                      EVERY
+                    </Typography>
+                    <TextField
+                      id="ep-markers-every-min-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeBtn}
+                      label="min"
+                      type="number"
+                      margin="dense"
+                    />
+                  </Grid>
+                  <Grid item xs={6} className={classes.grid}>
+                    <Typography variant="subheading" component="p">
+                      DURATION
+                    </Typography>
+                    <TextField
+                      id="ep-markers-every-duration-sec-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeBtn}
+                      label="sec"
+                      type="number"
+                      margin="dense"
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={2} className={classes.grid} />
+            </Grid>
+          </form>
         </Paper>
-      </Grid>
-    </Grid>
-  );
+      </div>
+    );
+  }
 }
-
-StreamForm.propTypes = {
-  classes: PropTypes.shape({
-    root: PropTypes.string.isRequired
-  }).isRequired
-};
 
 export default withStyles(styles)(StreamForm);
--- client/src/containers/CreateStream.jsx
+++ client/src/containers/CreateStream.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import PageHeading from '../components/PageHeading';
 import StreamForm from '../components/streams/StreamForm';
 
 function CreateStream() {
   return (
     <div>
-      <PageHeading title="Register DAI Stream" />
       <StreamForm />
     </div>
   );

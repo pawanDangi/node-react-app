53af8cf8dc0b0797557ecc9da42384513ae11590
Pawan Dangi <pdangi@intertrust.com>
2019-04-09 14:21:54 +0530

    updated on time format changes

--- client/src/utils/time-since.js
+++ client/src/utils/time-since.js
@@ -3,31 +3,22 @@ import moment from 'moment';
 const timeSince = date => {
   const seconds = Math.floor((moment(new Date()) - moment(date)) / 1000);
 
-  var interval = Math.floor(seconds / 31536000);
-  if (interval > 1) {
+  let interval = Math.floor(seconds / 3600);
+  if (interval > 1 && interval <= 23) {
+    return `${interval} hours ago`;
+  }
+  if (interval > 23 && interval <= 47) {
+    return 'yesterday';
+  }
+  if (interval > 47) {
     return moment(date).format('MMM DD, YYYY');
-  }
-
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return moment(date).format('MMM DD, YYYY');
-  }
-
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return ' days ago';
-  }
-
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return interval + ' hours ago';
   }
 
   interval = Math.floor(seconds / 60);
   if (interval > 1) {
-    return interval + ' minutes ago';
+    return `${interval} minutes ago`;
   }
-  return Math.floor(seconds) + ' seconds ago';
+  return `${Math.floor(seconds)} seconds ago`;
 };
 
 export default timeSince;

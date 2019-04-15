f8569298185c8fdb999098a0af835cba54a38554
Pawan Dangi <pdangi@intertrust.com>
2019-04-15 13:11:30 +0530

    refactor scte35 code

--- helpers/scte35.js
+++ helpers/scte35.js
@@ -1,249 +1,230 @@
 const BitString = require('bitstring');
 
-const splice_time = function(bitstr) {
-  const ret = {};
-  ret.time_specified_flag = bitstr.readbits(1);
-  if (ret.time_specified_flag == 1) {
-    ret.reserved = bitstr.readbits(6);
-    ret.pts_time_one = bitstr.readbits(1);
-    ret.pts_time = bitstr.readbits(32) / 90000.0;
+const spliceTime = bitstr => {
+  let reserved = {};
+  const timeSpecifiedFlag = bitstr.readbits(1);
+  if (timeSpecifiedFlag === 1) {
+    reserved = bitstr.readbits(6);
   } else {
-    ret.reserved = bitstr.readbits(7);
+    reserved = bitstr.readbits(7);
   }
 
-  return ret;
+  return { reserved };
 };
 
-const break_duration = function(bitstr) {
-  const ret = {};
+const breakDuration = bitstr => {
+  const reserved = bitstr.readbits(6);
+  const duration = bitstr.readbits(32) / 90000.0;
 
-  ret.auto_return = bitstr.readbits(1);
-  ret.reserved = bitstr.readbits(6);
-  ret.duration_one = bitstr.readbits(1);
-  ret.duration = bitstr.readbits(32) / 90000.0;
-
-  return ret;
+  return { reserved, duration };
 };
 
-const decode = function(scte_b64) {
-  const buf = Buffer.from(scte_b64, 'base64');
+const decode = scteB64 => {
+  const buf = Buffer.from(scteB64, 'base64');
 
   const retObj = {};
   const bitstr = new BitString('', buf);
 
-  retObj.byte_size = bitstr.size();
+  retObj.byteSize = bitstr.size();
 
   bitstr.seek(0, BitString.SEEK_ABSOLUTE);
 
-  retObj.table_id = bitstr.readbits(8);
+  retObj.tableId = bitstr.readbits(8);
 
-  if (retObj.table_id != 252) {
+  if (retObj.tableId !== 252) {
     // 0xfc
-    retObj.error = 'Invalid table_id.';
+    retObj.error = 'Invalid tableId.';
     return retObj;
   }
 
-  retObj.section_syntax_indicator = bitstr.readbits(1);
-  retObj.private_indicator = bitstr.readbits(1);
+  retObj.sectionSyntaxIndicator = bitstr.readbits(1);
+  retObj.privateIndicator = bitstr.readbits(1);
   retObj.reserved = bitstr.readbits(2);
-  retObj.section_length = bitstr.readbits(12);
+  retObj.sectionLength = bitstr.readbits(12);
 
-  if (retObj.section_length != retObj.byte_size - 3) {
+  if (retObj.sectionLength !== retObj.byteSize - 3) {
     // 3 bytes are already read at the start
-    retObj.error = 'Invalid section_length detected';
+    retObj.error = 'Invalid sectionLength detected';
     return retObj;
   }
 
   let i;
   let j;
 
-  retObj.protocol_version = bitstr.readbits(8);
+  retObj.protocolVersion = bitstr.readbits(8);
   retObj.encrypted = bitstr.readbits(1);
-  retObj.encryption_algorithm = bitstr.readbits(6);
-  retObj.pts_adjustment = bitstr.readbits(33);
+  retObj.encryptionAlgorithm = bitstr.readbits(6);
+  retObj.ptsAdjustment = bitstr.readbits(33);
 
-  retObj.cw_index = bitstr.readbits(8);
+  retObj.cwIndex = bitstr.readbits(8);
 
   retObj.tier = bitstr.readbits(12);
-  retObj.splice_command_length = bitstr.readbits(12);
-  retObj.splice_command_type = bitstr.readbits(8);
+  retObj.spliceCommandLength = bitstr.readbits(12);
+  retObj.spliceCommandType = bitstr.readbits(8);
 
-  if (retObj.splice_command_type === 5) {
+  if (retObj.spliceCommandType === 5) {
     // splice insert
-    retObj.splice_event_id = bitstr.readbits(32);
-    retObj.splice_event_cancel_indicator = bitstr.readbits(1);
-    retObj.splice_reserved = bitstr.readbits(7);
-    if (retObj.splice_event_cancel_indicator == 0) {
-      retObj.out_of_network_indicator = bitstr.readbits(1);
-      retObj.program_splice_flag = bitstr.readbits(1);
-      retObj.duration_flag = bitstr.readbits(1);
-      retObj.splice_immediate_flag = bitstr.readbits(1);
-      retObj.seci_reserved = bitstr.readbits(4);
-      if (
-        retObj.program_splice_flag === 1 &&
-        retObj.splice_immediate_flag === 0
-      ) {
-        retObj.splice_time = splice_time(bitstr);
+    retObj.spliceEventId = bitstr.readbits(32);
+    retObj.spliceEventCancelIndicator = bitstr.readbits(1);
+    retObj.spliceReserved = bitstr.readbits(7);
+    if (retObj.spliceEventCancelIndicator === 0) {
+      retObj.outOfNetworkIndicator = bitstr.readbits(1);
+      retObj.programSpliceFlag = bitstr.readbits(1);
+      retObj.durationFlag = bitstr.readbits(1);
+      retObj.spliceImmediateFlag = bitstr.readbits(1);
+      retObj.seciReserved = bitstr.readbits(4);
+      if (retObj.programSpliceFlag === 1 && retObj.spliceImmediateFlag === 0) {
+        retObj.spliceTime = spliceTime(bitstr);
       }
-      if (retObj.program_splice_flag === 0) {
-        retObj.component_count = bitstr.readbits(8);
-        for (i = 0; i < retObj.component_count; i++) {
-          retObj.splice_flag = [];
-          var x = {};
-          x.component_tag = bitstr.readbits(8);
-          x.splice_time = splice_time(bitstr);
-          retObj.splice_flag.push(x);
+      if (retObj.programSpliceFlag === 0) {
+        retObj.componentCount = bitstr.readbits(8);
+        for (i = 0; i < retObj.componentCount; i++) {
+          retObj.spliceFlag = [];
+          const x = {};
+          x.componentTag = bitstr.readbits(8);
+          x.spliceTime = spliceTime(bitstr);
+          retObj.spliceFlag.push(x);
         }
       }
-      if (retObj.duration_flag == 1) {
-        retObj.break_duration = break_duration(bitstr);
+      if (retObj.durationFlag === 1) {
+        retObj.breakDuration = breakDuration(bitstr);
       }
 
-      retObj.unique_program_id = bitstr.readbits(16);
-      retObj.avail_num = bitstr.readbits(8);
-      retObj.avail_expected = bitstr.readbits(8);
+      retObj.uniqueProgramId = bitstr.readbits(16);
+      retObj.availNum = bitstr.readbits(8);
+      retObj.availExpected = bitstr.readbits(8);
     }
   }
 
-  if (retObj.splice_command_type == 0) {
-    // console.log("null splice_command_type");
+  if (retObj.spliceCommandType === 0) {
+    // console.log("null spliceCommandType");
     return retObj;
   }
 
-  if (retObj.splice_command_type == 4) {
-    x.splice_count = bitstr.readbits(8);
-    x.splice_details = [];
-    for (i = 0; i < x.splice_count; i++) {
-      const g = new Object();
-      g.splice_event_id = bitstr.readbits(32);
-      g.splice_event_cancel_indicator = bitstr.readbits(1);
+  if (retObj.spliceCommandType === 4) {
+    x.spliceCount = bitstr.readbits(8);
+    x.spliceDetails = [];
+    for (i = 0; i < x.spliceCount; i++) {
+      const g = {};
+      g.spliceEventId = bitstr.readbits(32);
+      g.spliceEventCancelIndicator = bitstr.readbits(1);
       g.reserved = bitstr.readbits(7);
-      if (g.splice_event_cancel_indicator == 0) {
-        g.out_of_network_indicator = bitstr.readbits(1);
-        g.program_splice_flag = bitstr.readbits(1);
-        g.duration_flag = bitstr.readbits(1);
+      if (g.spliceEventCancelIndicator === 0) {
+        g.outOfNetworkIndicator = bitstr.readbits(1);
+        g.programSpliceFlag = bitstr.readbits(1);
+        g.durationFlag = bitstr.readbits(1);
         g.reserved = bitstr.readbits(5);
-        if (g.program_splice_flag == 1) {
-          g.utc_splice_time = bitstr.readbits(32);
+        if (g.programSpliceFlag === 1) {
+          g.utcSpliceTime = bitstr.readbits(32);
         }
-        if (g.program_splice_flag == 0) {
-          g.component_count = bitstr.readbits(8);
+        if (g.programSpliceFlag === 0) {
+          g.componentCount = bitstr.readbits(8);
           g.components = [];
-          for (j = 0; j < g.component_count; j++) {
-            const k = new Object();
-            k.component_tag = bitstr.readbits(8);
-            k.utc_splice_time = bitstr.readbits(32);
+          for (j = 0; j < g.componentCount; j++) {
+            const k = {};
+            k.componentTag = bitstr.readbits(8);
+            k.utcSpliceTime = bitstr.readbits(32);
             g.components.push(k);
           }
         }
 
-        if (g.duration_flag) {
-          g.break_duration = break_duration(bitstr);
+        if (g.durationFlag) {
+          g.breakDuration = breakDuration(bitstr);
         }
 
-        g.unique_program_id = bitstr.readbits(32);
-        g.avail_num = bitstr.readbits(8);
-        g.avails_expected = bitstr.readbits(8);
+        g.uniqueProgramId = bitstr.readbits(32);
+        g.availNum = bitstr.readbits(8);
+        g.availsExpected = bitstr.readbits(8);
       }
-      x.splice_details.push(g);
+      x.spliceDetails.push(g);
     }
   }
 
-  if (retObj.splice_command_type == 6) {
-    retObj.splice_time = splice_time(bitstr);
+  if (retObj.spliceCommandType === 6) {
+    retObj.spliceTime = spliceTime(bitstr);
   }
 
-  if (retObj.splice_command_type == 7) {
-    // TODO :  implement bandwidth_reservation()
+  if (retObj.spliceCommandType === 7) {
+    // TODO :  implement bandwidthReservation()
     return retObj;
   }
 
-  if (retObj.splice_command_type == 255) {
-    // TODO : private_command();
+  if (retObj.spliceCommandType === 255) {
+    // TODO : privateCommand();
     return retObj;
   }
 
-  retObj.descriptor_loop_length = bitstr.readbits(16);
+  retObj.descriptorLoopLength = bitstr.readbits(16);
   retObj.descriptor = [];
-  for (i = 0; i < retObj.descriptor_loop_length; i++) {
-    var x = new Object();
-    x.splice_descriptor_tag = bitstr.readbits(8);
-    x.descriptor_length = bitstr.readbits(8);
-    if (x.splice_descriptor_tag != 2) {
-      const useless = bitstr.readbits(x.descriptor_length);
+  for (i = 0; i < retObj.descriptorLoopLength; i++) {
+    const x = {};
+    x.spliceDescriptorTag = bitstr.readbits(8);
+    x.descriptorLength = bitstr.readbits(8);
+    if (x.spliceDescriptorTag !== 2) {
+      const useless = bitstr.readbits(x.descriptorLength);
     } else {
       // first two fields are the same for all, and they are already read above
-      // x.splice_descriptor_tag = bitstr.readbits(8);
-      // x.descriptor_length = bitstr.readbits(8);
+      // x.spliceDescriptorTag = bitstr.readbits(8);
+      // x.descriptorLength = bitstr.readbits(8);
       x.identifier = bitstr.readbits(32);
-      x.segmentation_event_id = bitstr.readbits(32);
-      x.segmentation_event_cancel_indicator = bitstr.readbits(1);
+      x.segmentationEventId = bitstr.readbits(32);
+      x.segmentationEventCancelIndicator = bitstr.readbits(1);
       x.reserved = bitstr.readbits(7);
 
-      if (x.segmentation_event_cancel_indicator == 0) {
-        x.program_segmentation_flag = bitstr.readbits(1);
-        x.segmentation_duration_flag = bitstr.readbits(1);
-        x.delivery_not_restricted_flag = bitstr.readbits(1);
+      if (x.segmentationEventCancelIndicator === 0) {
+        x.programSegmentationFlag = bitstr.readbits(1);
+        x.segmentationDurationFlag = bitstr.readbits(1);
+        x.deliveryNotRestrictedFlag = bitstr.readbits(1);
 
-        if (x.delivery_not_restricted_flag == 0) {
-          x.web_delivery_allowed_flag = bitstr.readbits(1);
-          x.no_regional_blackout_flag = bitstr.readbits(1);
-          x.archive_allowed_flag = bitstr.readbits(1);
-          x.device_restrictions = bitstr.readbits(2);
+        if (x.deliveryNotRestrictedFlag === 0) {
+          x.webDeliveryAllowedFlag = bitstr.readbits(1);
+          x.noRegionalBlackoutFlag = bitstr.readbits(1);
+          x.archiveAllowedFlag = bitstr.readbits(1);
+          x.deviceRestrictions = bitstr.readbits(2);
         } else {
-          x.reserved_2 = bitstr.readbits(5);
+          x.reserved2 = bitstr.readbits(5);
         }
-        if (x.program_segmentation_flag == 0) {
-          x.component_count = bitstr.readbits(8);
+        if (x.programSegmentationFlag === 0) {
+          x.componentCount = bitstr.readbits(8);
           x.components = [];
-          for (j = 0; j < x.component_count; j++) {
-            const y = new Object();
-            y.component_tag = bitstr.readbits(8);
+          for (j = 0; j < x.componentCount; j++) {
+            const y = {};
+            y.componentTag = bitstr.readbits(8);
             y.reserved = bitstr.readbits(7);
-            y.pts_offset = bitstr.readbits(33);
+            y.ptsOffset = bitstr.readbits(33);
             x.components.push(y);
           }
         }
 
-        if (x.segmentation_duration_flag == 1) {
-          x.segmentation_duration = bitstr.readbits(40);
+        if (x.segmentationDurationFlag === 1) {
+          x.segmentationDuration = bitstr.readbits(40);
         }
-        x.segmentation_upid_type = bitstr.readbits(8);
-        x.segmentation_upid_length = bitstr.readbits(8);
-        // TODO :  segmentation_upid(); :: check page 54
+        x.segmentationUpidType = bitstr.readbits(8);
+        x.segmentationUpidLength = bitstr.readbits(8);
+        // TODO :  segmentationUpid(); :: check page 54
 
-        x.segmentation_type_id = bitstr.readbits(8);
-        x.segment_num = bitstr.readbits(8);
-        x.segments_expected = bitstr.readbits(8);
+        x.segmentationTypeId = bitstr.readbits(8);
+        x.segmentNum = bitstr.readbits(8);
+        x.segmentsExpected = bitstr.readbits(8);
 
-        if (x.segmentation_type_id == 52 || x.segmentation_type_id == 54) {
+        if (x.segmentationTypeId === 52 || x.segmentationTypeId === 54) {
           // 0x34 => 52, 0x36 => 54
-          x.sub_segment_num = bitstr.readbits(8);
-          x.sub_segments_expected = bitstr.readbits(8);
+          x.subSegmentNum = bitstr.readbits(8);
+          x.subSegmentsExpected = bitstr.readbits(8);
         }
       }
       retObj.descriptor.push(x);
     }
   }
 
-  if (retObj.encrypted != 0) {
-    // do something about the alignment stuffing
-  }
-
-  if (retObj.encrypted == 1) {
-    retObj.e_crc32 = bitstr.readbits(32);
+  if (retObj.encrypted === 1) {
+    retObj.eCrc32 = bitstr.readbits(32);
   }
 
   retObj.crc32 = bitstr.readbits(32);
 
   return retObj;
 };
-
-/*
-var parse = function(sctecode, callback) {
-
-};
-*/
 
 // module.exports.parse = parse;
 module.exports.decode = decode;

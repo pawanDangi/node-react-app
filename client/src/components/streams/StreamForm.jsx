7c3e2d78cf5c292f7eb8a3064b9a903ea8297474
Pawan Dangi <pdangi@intertrust.com>
2019-04-18 15:05:16 +0530

    create stream page design

--- client/src/components/streams/StreamForm.jsx
+++ client/src/components/streams/StreamForm.jsx
@@ -1,128 +1,145 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import {
   Paper,
   Typography,
   Grid,
   TextField,
   Button,
-  MenuItem,
-  Fab
-} from "@material-ui/core/";
-import { Add as AddIcon } from "@material-ui/icons/";
-import className from "class-names";
+  MenuItem
+} from '@material-ui/core/';
+import { Add as AddIcon } from '@material-ui/icons/';
+import className from 'class-names';
 
-import { background, text, primary } from "../../utils/colors";
+import { background, text, primary } from '../../utils/colors';
 
 const styles = theme => ({
   root: {
-    "& div": {
+    marginBottom: '10px',
+    '& div': {
       background: background.paper
     }
   },
   page: {
     ...theme.mixins.gutters(),
-    [theme.breakpoints.down("xs")]: {
-      width: "90%",
-      margin: "0 5%"
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+      margin: '0 5%'
     },
-    [theme.breakpoints.up("sm")]: {
-      width: "90%",
-      margin: "0 5%"
+    [theme.breakpoints.up('sm')]: {
+      width: '90%',
+      margin: '0 5%'
     },
-    [theme.breakpoints.up("md")]: {
-      width: "80%",
-      margin: "0 10%"
+    [theme.breakpoints.up('md')]: {
+      width: '80%',
+      margin: '0 10%'
     },
-    [theme.breakpoints.up("lg")]: {
-      width: "70%",
-      margin: "0 15%"
+    [theme.breakpoints.up('lg')]: {
+      width: '70%',
+      margin: '0 15%'
     },
-    padding: "15px !important"
+    padding: '15px !important'
   },
   heading: {
     borderBottom: `2px solid ${text.disabled}`,
     paddingBottom: 20,
-    fontSize: "18px"
+    fontSize: '18px'
   },
   title: {
-    padding: "10px 0px !important",
+    padding: '10px 0px !important',
     color: primary.light,
     fontWeight: 600,
-    fontSize: "18px"
+    fontSize: '18px'
   },
   form: {
-    padding: "20px 0 0"
+    padding: '20px 0 0'
   },
   grid: {
-    paddingTop: "5px !important",
-    paddingBottom: "5px !important",
-    display: "flex",
-    alignItems: "center",
-    "& div": {
-      background: "none"
+    paddingTop: '5px !important',
+    paddingBottom: '5px !important',
+    display: 'flex',
+    alignItems: 'center',
+    '& div': {
+      background: 'none'
     }
   },
   textField: {
     margin: 0
   },
   validate: {
-    alignItems: "center",
-    display: "flex"
+    alignItems: 'center',
+    display: 'flex'
   },
   validateBtn: {
     background: primary.main,
     color: primary.contrastText,
-    "&:hover": {
+    '&:hover': {
       background: primary.dark
     },
-    "&:focus": {
-      textDecoration: "none",
-      outline: "none"
+    '&:focus': {
+      textDecoration: 'none',
+      outline: 'none'
     }
   },
   addMarker: {
     background: primary.main,
     color: primary.contrastText,
-    "&:hover": {
+    '&:hover': {
       background: primary.dark
     },
-    "&:focus": {
-      textDecoration: "none",
-      outline: "none"
+    '&:focus': {
+      textDecoration: 'none',
+      outline: 'none'
     }
   },
   timeInput: {
-    width: "50px",
-    marginRight: "5px"
+    width: '50%',
+    marginRight: '5px'
   },
   timeTxt: {
-    paddingRight: "5px"
+    paddingRight: '5px'
   },
   or: {
-    width: "100%",
-    textAlign: "center",
+    width: '100%',
+    textAlign: 'center',
     borderBottom: `1px solid ${text.disabled}`,
-    lineHeight: "0.1em",
-    margin: "10px 0 20px",
-    paddingTop: "15px",
-    fontSize: "15px",
-    "& span": {
+    lineHeight: '0.1em',
+    margin: '0',
+    paddingTop: '0',
+    fontSize: '15px',
+    '& span': {
       background: primary.contrastText,
       color: `${text.disabled}`,
-      padding: "0 10px"
+      padding: '0 10px'
+    }
+  },
+  action: {
+    justifyContent: 'flex-end'
+  },
+  actionButton: {
+    margin: '0 10px'
+  },
+  save: {
+    background: primary.main,
+    color: primary.contrastText,
+    '&:hover': {
+      background: primary.dark
+    },
+    '&:focus': {
+      textDecoration: 'none',
+      outline: 'none'
     }
   }
 });
 
 class StreamForm extends Component {
   state = {
-    name: "",
-    url: "",
-    domain: "",
-    format: "",
-    type: "",
-    tags: ""
+    name: '',
+    url: '',
+    domain: '',
+    format: '',
+    type: '',
+    tags: ''
   };
 
   componentWillReceiveProps(props) {
@@ -140,7 +157,6 @@ class StreamForm extends Component {
   render() {
     const { classes, validateStream } = this.props;
     const { name, url, domain, format, type, tags } = this.state;
-    console.log(type);
 
     return (
       <div className={classes.root}>
@@ -168,7 +184,7 @@ class StreamForm extends Component {
                   margin="dense"
                   variant="outlined"
                   value={name}
-                  onChange={e => this.onChange("name", e)}
+                  onChange={e => this.onChange('name', e)}
                 />
               </Grid>
               <Grid item sm={10} xs={12} className={classes.grid}>
@@ -180,7 +196,7 @@ class StreamForm extends Component {
                   margin="dense"
                   variant="outlined"
                   value={url}
-                  onChange={e => this.onChange("url", e)}
+                  onChange={e => this.onChange('url', e)}
                 />
               </Grid>
               <Grid
@@ -208,7 +224,7 @@ class StreamForm extends Component {
                   margin="dense"
                   variant="outlined"
                   value={domain}
-                  onChange={e => this.onChange("domain", e)}
+                  onChange={e => this.onChange('domain', e)}
                 />
               </Grid>
               <Grid item sm={5} xs={12} className={classes.grid}>
@@ -221,12 +237,12 @@ class StreamForm extends Component {
                   variant="outlined"
                   select
                   value={format}
-                  onChange={e => this.onChange("format", e)}
+                  onChange={e => this.onChange('format', e)}
                 >
-                  <MenuItem key={"hls"} value={"HLS"}>
+                  <MenuItem key={'hls'} value={'HLS'}>
                     HLS
                   </MenuItem>
-                  <MenuItem key={"dash"} value={"DASH"}>
+                  <MenuItem key={'dash'} value={'DASH'}>
                     DASH
                   </MenuItem>
                 </TextField>
@@ -241,15 +257,15 @@ class StreamForm extends Component {
                   variant="outlined"
                   select
                   value={type}
-                  onChange={e => this.onChange("type", e)}
+                  onChange={e => this.onChange('type', e)}
                 >
-                  <MenuItem key={"vod"} value={"VOD"}>
+                  <MenuItem key={'vod'} value={'VOD'}>
                     VOD
                   </MenuItem>
-                  <MenuItem key={"live"} value={"LIVE"}>
+                  <MenuItem key={'live'} value={'LIVE'}>
                     LIVE
                   </MenuItem>
-                  <MenuItem key={"event"} value={"EVENT"}>
+                  <MenuItem key={'event'} value={'EVENT'}>
                     EVENT
                   </MenuItem>
                 </TextField>
@@ -263,7 +279,7 @@ class StreamForm extends Component {
                   margin="dense"
                   variant="outlined"
                   value={tags}
-                  onChange={e => this.onChange("tags", e)}
+                  onChange={e => this.onChange('tags', e)}
                 />
               </Grid>
               <Grid item xs={12} className={classes.grid}>
@@ -275,6 +291,61 @@ class StreamForm extends Component {
                   AD MARKUPS
                 </Typography>
               </Grid>
+              <Grid item md={2} xs={12} className={classes.grid}>
+                <Typography variant="subheading" component="p">
+                  CUE OUT/IN :
+                </Typography>
+              </Grid>
+              <Grid item md={8} xs={10} className={classes.grid}>
+                <Grid container spacing={8}>
+                  <Grid item xs={6} className={classes.grid}>
+                    <Typography
+                      className={classes.timeTxt}
+                      variant="subheading"
+                      component="p"
+                    >
+                      START
+                    </Typography>
+                    <TextField
+                      id="ep-markers-start-min-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeInput}
+                      label="min"
+                      type="number"
+                      margin="dense"
+                    />
+                    <TextField
+                      id="ep-markers-start-sec-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeInput}
+                      label="sec"
+                      type="number"
+                      margin="dense"
+                    />
+                  </Grid>
+                  <Grid item xs={6} className={classes.grid}>
+                    <Typography
+                      className={classes.timeTxt}
+                      variant="subheading"
+                      component="p"
+                    >
+                      DURATION
+                    </Typography>
+                    <TextField
+                      id="ep-markers-duration-sec-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeInput}
+                      type="number"
+                      label="sec"
+                      margin="dense"
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={2} className={classes.grid} />
               <Grid item md={2} xs={12} className={classes.grid}>
                 <Typography variant="subheading" component="p">
                   EP MARKERS :
@@ -330,14 +401,14 @@ class StreamForm extends Component {
                 </Grid>
               </Grid>
               <Grid item xs={2} className={classes.grid}>
-                <Fab
+                <Button
                   size="small"
                   color="primary"
                   aria-label="Add"
                   className={classes.addMarker}
                 >
                   <AddIcon />
-                </Fab>
+                </Button>
               </Grid>
               <Grid item md={2} xs={12} className={classes.grid} />
               <Grid item md={8} xs={10} className={classes.grid}>
@@ -388,6 +459,75 @@ class StreamForm extends Component {
                 </Grid>
               </Grid>
               <Grid item xs={2} className={classes.grid} />
+              <Grid item md={2} xs={12} className={classes.grid}>
+                <Typography variant="subheading" component="p">
+                  AUTO DETECT :
+                </Typography>
+              </Grid>
+              <Grid item md={8} xs={10} className={classes.grid}>
+                <Grid container spacing={8}>
+                  <Grid item xs={6} className={classes.grid}>
+                    <Typography
+                      className={classes.timeTxt}
+                      variant="subheading"
+                      component="p"
+                    >
+                      START
+                    </Typography>
+                    <TextField
+                      id="ep-markers-start-min-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeInput}
+                      label="min"
+                      type="number"
+                      margin="dense"
+                    />
+                    <TextField
+                      id="ep-markers-start-sec-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeInput}
+                      label="sec"
+                      type="number"
+                      margin="dense"
+                    />
+                  </Grid>
+                  <Grid item xs={6} className={classes.grid}>
+                    <Typography
+                      className={classes.timeTxt}
+                      variant="subheading"
+                      component="p"
+                    >
+                      DURATION
+                    </Typography>
+                    <TextField
+                      id="ep-markers-duration-sec-input"
+                      variant="outlined"
+                      fullWidth
+                      className={classes.timeInput}
+                      type="number"
+                      label="sec"
+                      margin="dense"
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                className={className(classes.grid, classes.action)}
+              >
+                <Button variant="contained" className={classes.actionButton}>
+                  Cancel
+                </Button>
+                <Button
+                  variant="contained"
+                  className={className(classes.actionButton, classes.save)}
+                >
+                  Save
+                </Button>
+              </Grid>
             </Grid>
           </form>
         </Paper>
